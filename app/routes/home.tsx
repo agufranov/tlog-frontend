64a86673832c6abd9f8853a03cc1a8013e8bbb4b
agufranov <al.gufranov@gmail.com>
2025-07-20 06:28:53 +0300

    add chakraUi and zustand

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home'
+import { Welcome } from '../welcome/welcome'
+import { useEffect } from 'react'
+import { useFetchJson } from '@/hooks/useFetchJson'
+import { Provider } from '@/components/ui/provider'
+import { SplashScreen } from '@/components/splashScreen'
+import { useUserStore, type UserState } from '@/store/user'
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return [{ title: 'The last of Guss' }, { name: 'description', content: 'The last of Guss game' }]
 }
 
 export default function Home() {
-  return <Welcome />;
+  const user = useUserStore((state) => state.x)
+
+  return (
+    <Provider>
+      <div className="center ml-16">{user ? <SplashScreen /> : <Welcome />}</div>
+    </Provider>
+  )
 }
