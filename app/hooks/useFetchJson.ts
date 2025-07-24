190577b8393473bab77fa7b9cb9fa3e23179a31d
agufranov <al.gufranov@gmail.com>
2025-07-24 15:53:09 +0300

    useFetchJson refactor & cleanup

--- app/hooks/useFetchJson.ts
+++ app/hooks/useFetchJson.ts
@@ -3,9 +3,7 @@ import {
   fetchJson,
   type FetchArguments,
   type FetchJsonMethodWithBody,
-  type FetchJsonMethodWithBodyArguments,
   type FetchJsonMethodWithoutBody,
-  type FetchJsonMethodWithoutBodyArguments,
 } from '~/helpers/fetchJson'
 import { HTTP_METHODS, isHttpMethodHasBody, type HttpMethod, type HttpMethodsWithoutBody } from '~/helpers/httpMethods'
 
@@ -22,92 +20,77 @@ interface UseFetchJsonMethodWithPayloadResult<TPayload extends object, TResult e
   fetch: (payload?: TPayload) => Promise<TResult>
 }
 
-interface UseFetchJsonMethodWithPayloadResult<TPayload extends object, TResult extends object>
-  extends UseFetchJsonMethodWithoutPayloadResult<TResult> {
-  fetch: (payload?: TPayload) => Promise<TResult>
+type UseFetchJson = {
+  [httpMethod in HttpMethod]: ReturnType<typeof createUseFetchJson>
 }
 
-type UseFetchJsonMethodWithBody = <TPayload extends object = any, TResult extends object = any>(
-  input: FetchJsonMethodWithBodyArguments<TPayload>[0],
-  payload?: FetchJsonMethodWithBodyArguments<TPayload>[1],
-  init?: FetchJsonMethodWithBodyArguments<TPayload>[2]
-) => UseFetchJsonMethodWithPayloadResult<TPayload, TResult>
+const isMethodHasBody = (
+  httpMethod: HttpMethod,
+  method: FetchJsonMethodWithBody | FetchJsonMethodWithoutBody
+): method is FetchJsonMethodWithBody => {
+  return isHttpMethodHasBody(httpMethod)
+}
 
-type UseFetchJsonMethodWithoutBody = <TResult extends object = any>(
-  input: FetchJsonMethodWithoutBodyArguments[0],
-  init?: FetchJsonMethodWithoutBodyArguments[1]
-) => UseFetchJsonMethodWithoutPayloadResult<TResult>
+const createUseFetchJson = (httpMethod: HttpMethod) => {
+  return <TPayload extends object, TResult extends object>(...args: FetchArguments) => {
+    type UseFetchJsonMethodResult = typeof httpMethod extends HttpMethodsWithoutBody
+      ? UseFetchJsonMethodWithoutPayloadResult<TResult>
+      : UseFetchJsonMethodWithPayloadResult<TPayload, TResult>
 
-export type FetchJsonMethodArguments<M extends HttpMethod, TPayload extends object> = M extends HttpMethodsWithoutBody
-  ? FetchJsonMethodWithoutBodyArguments
-  : FetchJsonMethodWithBodyArguments<TPayload>
+    const [value, setValue] = useState<UseFetchJsonMethodResult['value']>()
+    const [error, setError] = useState<UseFetchJsonMethodResult['error']>()
+    const [loading, setLoading] = useState<UseFetchJsonMethodResult['loading']>(false)
+    const [isFetched, setIsFetched] = useState<UseFetchJsonMethodResult['isFetched']>(false)
 
-type UseFetchJson = {
-  [httpMethod in HttpMethod]: httpMethod extends HttpMethodsWithoutBody
-    ? UseFetchJsonMethodWithoutBody
-    : UseFetchJsonMethodWithBody
+    const fetchData = useCallback(
+      async (payload: TPayload) => {
+        setError(undefined)
+        setLoading(true)
+
+
+        try {
+          const fetchJsonMethod = fetchJson[httpMethod]
+
+          const hasBody = isMethodHasBody(httpMethod, fetchJsonMethod)
+
+          const fetchResult = await (hasBody
+            ? fetchJsonMethod(args[0], payload, args[1])
+            : fetchJsonMethod(args[0], args[1]))
+
+          const data = fetchResult.data as TResult
+          // response = fetchResult.response
+
+          setValue(data)
+
+          return data
+        } catch (err) {
+          setValue(undefined) // TODO спорно
+          setError(err)
+          throw err
+        } finally {
+          setLoading(false)
+          setIsFetched(true)
+        }
+      },
+      [setValue]
+    )
+
+    return {
+      value,
+      loading,
+      error,
+      isFetched,
+      fetch: fetchData,
+    }
+  }
 }
 
 export const useFetchJson = HTTP_METHODS.reduce((acc, httpMethod) => {
   return {
     ...acc,
-    [httpMethod]: <TPayload extends object, TResult extends object>(...args: FetchArguments) => {
-      type UseFetchJsonMethodResult = typeof httpMethod extends HttpMethodsWithoutBody
-        ? UseFetchJsonMethodWithoutPayloadResult<TResult>
-        : UseFetchJsonMethodWithPayloadResult<TPayload, TResult>
-
-      const [value, setValue] = useState<UseFetchJsonMethodResult['value']>()
-      const [error, setError] = useState<UseFetchJsonMethodResult['error']>()
-      const [loading, setLoading] = useState<UseFetchJsonMethodResult['loading']>(false)
-      const [isFetched, setIsFetched] = useState<UseFetchJsonMethodResult['isFetched']>(false)
-
-      const isMethodHasBody = <M extends HttpMethod>(
-        httpMethod: M,
-        method: FetchJsonMethodWithBody | FetchJsonMethodWithoutBody
-      ): method is FetchJsonMethodWithBody => {
-        return isHttpMethodHasBody(httpMethod)
-      }
-
-      const fetchData = useCallback(
-        async (payload: TPayload) => {
-          setError(undefined)
-          setLoading(true)
-          let data: TResult
-          let response: Response
-          try {
-            const fetchJsonMethod = fetchJson[httpMethod]
-
-            const hasBody = isMethodHasBody(httpMethod, fetchJsonMethod)
-
-            const fetchResult = await (hasBody
-              ? (fetchJsonMethod as FetchJsonMethodWithBody)(args[0], payload, args[1])
-              : (fetchJsonMethod as FetchJsonMethodWithoutBody)(args[0], args[1]))
-
-            data = fetchResult.data
-            response = fetchResult.response
-
-            setValue(data)
-
-            return data
-          } catch (err) {
-            setValue(undefined) // TODO спорно
-            setError(err)
-            throw err
-          } finally {
-            setLoading(false)
-            setIsFetched(true)
-          }
-        },
-        [setValue]
-      )
-
-      return {
-        value,
-        loading,
-        error,
-        isFetched,
-        fetch: fetchData,
-      }
-    },
+    [httpMethod]: createUseFetchJson(httpMethod),
   }
 }, {} as UseFetchJson)
+
+const sss = useFetchJson.post<{ s: string }, { a: number }>('/api/auth/profile')
+sss.fetch({ s: 's' }).then((res) => res.a)
