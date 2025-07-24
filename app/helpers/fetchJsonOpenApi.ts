d35e92d13fc2ce46e491bbfad9e86af2914e1b62
agufranov <al.gufranov@gmail.com>
2025-07-24 14:47:03 +0300

    Restriction of routes by method works!

--- app/helpers/fetchJsonOpenApi.ts
+++ app/helpers/fetchJsonOpenApi.ts
@@ -1,11 +1,10 @@
-import type { paths } from 'api'
 import { fetchJson } from './fetchJson'
 import { HTTP_METHODS, isHttpMethodHasBody, type HttpMethod } from './httpMethods'
-import type { ApiEndpointRequest, ApiEndpointResponseSuccess } from './openApiTypes'
+import type { ApiEndpointRequest, ApiEndpointResponseSuccess, RoutesWithMethod } from './openApiTypes'
 
 const createFetchJsonOpenApiMethod = <M extends HttpMethod>(httpMethod: M) => {
   if (isHttpMethodHasBody(httpMethod)) {
-    return async <TPath extends keyof paths>(
+    return async <TPath extends RoutesWithMethod<M>>(
       input: TPath,
       payload?: ApiEndpointRequest<M, TPath>,
       init?: RequestInit
@@ -14,7 +13,7 @@ const createFetchJsonOpenApiMethod = <M extends HttpMethod>(httpMethod: M) => {
       return data as Promise<ApiEndpointResponseSuccess<M, TPath>>
     }
   } else {
-    return async <TPath extends keyof paths>(input: TPath, init?: RequestInit) => {
+    return async <TPath extends RoutesWithMethod<M>>(input: TPath, init?: RequestInit) => {
       const { data } = await fetchJson[httpMethod].call(null, input, init)
       return data as Promise<ApiEndpointResponseSuccess<M, TPath>>
     }
@@ -48,18 +47,44 @@ const yyy = fetchJsonOpenApi
 type E = {
   e1: {
     get?: never
+    post: {}
   }
   e2: {
     get: {}
+    post: {}
   }
   e3: {
     get?: never
+    post?: never
   }
   e4: {
     get: {}
+    post?: never
   }
 }
 
-type GettableKeys = keyof { [K in keyof E as E[K]['get'] extends never | undefined ? never : K]: K }
+type MS = 'get' | 'post'
 
-const gkk: GettableKeys = 'e2'
+type AvailableKeys<Method extends MS> = {
+  [K in keyof E]-?: E[K] extends { [M in Method]: {} } ? K : never // Iterate through all keys of E and remove optionality // Check if K has a property with the given Method which is of type {}
+}[keyof E] // Get all the keys that satisfy the condition
+
+type T<M extends 'get' | 'post'> = AvailableKeys<M>
+
+// Test:
+type GetKeys = T<'get'> // "e2" | "e4"
+type PostKeys = T<'post'> // "e1" | "e2"
+
+// f('get','e1') // дожна быть ошибка typescript: E.e1.get имеет тип never | undefined
+// f('post', 'e1') // ok
+// f('get', 'e2') // ok
+
+// type GettableKeys<O, S extends keyof E[keyof E]> = keyof {
+//   [K in keyof O as O[K][S] extends never | undefined ? never : K]: K
+// }
+
+// type A = keyof E[keyof E]
+
+// const gkk: GettableKeys<E, 's'> = 'e2'
+
+// const post = <TPath extends keyof paths>(input: TPath) => {}
--- app/helpers/openApiTypes.ts
+++ app/helpers/openApiTypes.ts
@@ -3,28 +3,43 @@ import type { HttpMethod } from './httpMethods'
 
 type ApiRoute<T extends keyof paths> = paths[T]
 
-type ApiEndpoint<M extends HttpMethod, T extends keyof paths> = M extends keyof ApiRoute<T> ? ApiRoute<T>[M] : never
+// TODO rename
+type X<O, K> = K extends keyof O ? O[K] : never
 
-export type ApiEndpointRequest<M extends HttpMethod, T extends keyof paths> = 'requestBody' extends keyof ApiEndpoint<M, T>
-  ? 'content' extends keyof ApiEndpoint<M, T>['requestBody']
-    ? 'application/json' extends keyof ApiEndpoint<M, T>['requestBody']['content']
-      ? ApiEndpoint<M, T>['requestBody']['content']['application/json'] & {}
-      : never
-    : never
-  : never
+type ApiEndpoint<M extends HttpMethod, T extends keyof paths> = X<ApiRoute<T>, M>
+// type ApiEndpoint<M extends HttpMethod, T extends keyof paths> = M extends keyof ApiRouteName<T> ? ApiRouteName<T>[M] : never
 
-type ApiEndpointResponses<M extends HttpMethod, T extends keyof paths> = 'responses' extends keyof ApiEndpoint<M, T>
-  ? ApiEndpoint<M, T>['responses']
-  : never
-
-// ODO handle errors too
-export type ApiEndpointResponseSuccess<M extends HttpMethod, T extends keyof paths> = 200 extends keyof ApiEndpointResponses<
+export type ApiEndpointRequest<M extends HttpMethod, T extends keyof paths> = 'requestBody' extends keyof ApiEndpoint<
   M,
   T
 >
+  ? 'content' extends keyof ApiEndpoint<M, T>['requestBody']
+    ? X<ApiEndpoint<M, T>['requestBody']['content'], 'application/json'>
+    : never
+  : never
+
+type ApiEndpointResponses<M extends HttpMethod, T extends keyof paths> = X<ApiEndpoint<M, T>, 'responses'>
+
+// type ApiEndpointResponses<M extends HttpMethod, T extends keyof paths> = 'responses' extends keyof ApiEndpoint<M, T>
+//   ? ApiEndpoint<M, T>['responses']
+//   : never
+
+// ODO handle errors too
+export type ApiEndpointResponseSuccess<
+  M extends HttpMethod,
+  T extends keyof paths
+> = 200 extends keyof ApiEndpointResponses<M, T>
   ? 'content' extends keyof ApiEndpointResponses<M, T>[200]
     ? 'application/json' extends keyof ApiEndpointResponses<M, T>[200]['content']
       ? ApiEndpointResponses<M, T>[200]['content']['application/json'] & {}
       : never
     : never
   : never
+
+export type ApiRouteHasMethod<M extends HttpMethod, TPath extends keyof paths> = M extends keyof ApiRoute<TPath>
+  ? ApiRoute<TPath>[M]
+  : never
+
+export type RoutesWithMethod<M extends HttpMethod> = {
+  [P in keyof paths]: M extends keyof paths[P] ? (paths[P][M] extends {} ? P : never) : never
+}[keyof paths]
